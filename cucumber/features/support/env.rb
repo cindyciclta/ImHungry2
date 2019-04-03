93836e99f21c8c1f1c823d4626fdc62099947243
Veda Murthy <vvmurthy@usc.edu>
2019-04-03 12:40:42 -0700

    we'll see

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -1,10 +1,22 @@
 require 'capybara'
 require 'capybara/cucumber'
+require "selenium/webdriver"
 require 'rspec'
 
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"C:/Users/lazlo/Downloads/chromedriver.exe")
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/usr/local/bin/chromedriver")
 end
 
-Capybara.default_driver = :chrome
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    driver_path: "/usr/local/bin/chromedriver",
+    desired_capabilities: capabilities
+end
+
+Capybara.default_driver = :headless_chrome

372095056d9e1db952843e1fca7b935460884459
Veda Murthy <vvmurthy@usc.edu>
2019-04-08 09:25:13 -0700

    added headless fix

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -5,12 +5,12 @@ require 'rspec'
 
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/usr/local/bin/chromedriver")
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"C:\\Users\\veda\\Downloads\\chromedriver.exe")
 end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) }
+    chromeOptions: { args: %w(headless disable-gpu window-size=1280,800) }
   )
 
   Capybara::Selenium::Driver.new app,

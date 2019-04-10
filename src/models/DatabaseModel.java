cbc498482d6ecf98bedc1341398d96363a1f3aa2
Veda Murthy <vvmurthy@usc.edu>
2019-04-10 12:40:10 -0700

    trigger travis build

--- src/models/DatabaseModel.java
+++ src/models/DatabaseModel.java
@@ -15,7 +15,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class DatabaseModel {
-	static final String SQL_PASSWORD = "root"; // SET YOUR MYSQL PASSWORD HERE TO GET DATABASE WORKING!!!!!!!!
+	static final String SQL_PASSWORD = "root"; // SET YOUR MYSQL PASSWORD HERE TO GET DATABASE WORKING!!!!!!!
 	
 	public static int signInUser(String username, char[] password) throws Exception {
 		
